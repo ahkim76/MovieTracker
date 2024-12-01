d7255612ca22424dd97fe42990ffde60459d7e94
ahkim76 <ahkim9976@gmail.com>
2024-12-01 17:51:29 -0500

    Test

--- frontend/src/Components/MovieCard.jsx
+++ frontend/src/Components/MovieCard.jsx
@@ -1,5 +1,15 @@
-function MovieCard() {
-  return <div>Movie card</div>;
+import "../movies.css";
+function MovieCard({ movie }) {
+  return (
+    <li className="movieLi" key={movie.imdbID}>
+      <h3>{movie.Title}</h3>
+      <div className="movieDiv">
+        <p>Year: {movie.Year}</p>
+        <p>Type: {movie.Type}</p>
+      </div>
+      <img src={movie.Poster} alt={`${movie.Title} movie poster`} />
+    </li>
+  );
 }
 
 export default MovieCard;
--- frontend/src/Components/MovieList.jsx
+++ frontend/src/Components/MovieList.jsx
@@ -1,3 +1,4 @@
+import MovieCard from "./MovieCard";
 function MovieList({ movies }) {
   if (!movies || movies.length === 0) {
     return <p>No movies found. Try searching for something else!</p>;
@@ -6,12 +7,7 @@ function MovieList({ movies }) {
   return (
     <ul>
       {movies.map((movie) => (
-        <li key={movie.imdbID}>
-          <h3>{movie.Title}</h3>
-          <p>Year: {movie.Year}</p>
-          <p>Genre: {movie.Type}</p>
-          <img src={movie.Poster} alt={`${movie.Title} movie poster`} />
-        </li>
+        <MovieCard movie={movie} />
       ))}
     </ul>
   );
