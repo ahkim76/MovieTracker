db8fcd65af4668061f8f628cfcd6454a2c6c82bd
Justin <thisisjustinmao@gmail.com>
2024-12-01 21:24:58 -0500

    Working movie wishlist

--- frontend/src/Components/MovieWishlist.jsx
+++ frontend/src/Components/MovieWishlist.jsx
@@ -3,22 +3,6 @@ import { useState, useEffect, useCallback } from "react";
 import "../movies.css";
 
 function MovieWishlist() {
-  const getMovieIDs = async () => {
-    try {
-      const response = await fetch(`http://localhost:4000/movies`);
-
-      if (response.ok) {
-        let movieIDs = await response.json();
-
-        return movieIDs;
-      } else {
-        console.log("Error fetching movies");
-      }
-    } catch (error) {
-      console.log("Error getting movies");
-    }
-  };
-
   const getMovie = async (id) => {
     try {
       const response = await fetch(`http://localhost:4000/movies/id/${id}`);
@@ -35,32 +19,34 @@ function MovieWishlist() {
     }
   };
 
-  const getMovies = useCallback(async () => {
-    let movieData = [];
-    let movieIDs = await getMovieIDs();
-
-    if (movieIDs != null) {
-      for (let id of movieIDs) {
-        let data = await getMovie(id);
-
-        movieData.push(data);
-      }
-    }
-
-    return movieData;
-  }, []);
+  const [movieData, setMovieData] = useState([]);
 
   useEffect(() => {
-    getMovies();
-  }, [getMovies]);
+    const getMovies = async () => {
+        let movieData = [];
+        let movieIDs = localStorage.getItem("movies").split(",");
+    
+        if (movieIDs != null ) {
+          for (let id of movieIDs) {
+            let data = await getMovie(id);
+    
+            movieData.push(data);
+          }
+        }
+    
+        setMovieData(movieData);
+      };
 
-  if (getMovies.length === 0) {
+      getMovies();
+  }, []);  
+
+  if (movieData.length === 0) {
     return "Wishlist empty. Search for a movie to add to your wishlist!";
   }
 
   return (
     <ul className="wishlistParent">
-      {getMovies.map((movie) => (
+      {movieData.map((movie) => (
         <WishlistCard movie={movie} />
       ))}
     </ul>
