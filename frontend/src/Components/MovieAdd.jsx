6f1f56f47dcbad3d82c035f72b552c1b5e3683ce
ahkim76 <ahkim9976@gmail.com>
2024-12-01 20:40:18 -0500

    Fixed stuff

--- frontend/src/Components/MovieAdd.jsx
+++ frontend/src/Components/MovieAdd.jsx
@@ -3,18 +3,24 @@ function MovieAdd({ movie }) {
     let movies = [localStorage.getItem("movies")];
 
     if (movies == null) {
-        movies = [];
+      movies = [];
     }
 
     movies = [id, ...movies];
 
     localStorage.setItem("movies", movies);
-    console.log(localStorage.getItem("movies"));
-  }
+    console.log(localStorage.getItem("movies").split(","));
+  };
 
   return (
     <div>
-      <button onClick={() => { addMovie('tt6710474') } }>Add to wishlist</button>
+      <button
+        onClick={() => {
+          addMovie("tt6710474");
+        }}
+      >
+        Add to wishlist
+      </button>
     </div>
   );
 }
